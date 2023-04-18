57de91ae34e1ec9b9acea085858488df317f03e3
opilane <opilane@HP-CZC2349HTR>
2023-04-18 15:27:53 +0300

    qwe

--- _18.04.py
+++ _18.04.py
@@ -1,3 +1,4 @@
+
 from pickle import FALSE
 import pygame, sys
 
@@ -15,41 +16,18 @@ pygame.display.set_caption("Animatsion")
 ekraan.fill(roheline)
 kell=pygame.time.Clock()
 pudge=pygame.image.load("zxc.png")
-posX=0
-posY=0
+posX=X-pudge.get_rect().width
+posY=Y-pudge.get_rect().height
 lõpp=False 
 samm=2
-suund = "paremale"
 while not lõpp:
     kell.tick(60)
     events=pygame.event.get()
     for i in pygame.event.get():
-        if i.type==pygame.QUIT:
+        if i. type==pygame.QUIT():
             sys.exit()
-            
-    if suund == "paremale":
-        if posX >= X - pudge.get_rect().width:
-            suund = "alla"
-        else:
-            posX += samm
-    elif suund == "alla":
-        if posY >= Y - pudge.get_rect().height:
-            suund = "vasakule"
-        else:
-            posY += samm
-    elif suund == "vasakule":
-        if posX <= 0:
-            suund = "üles"
-        else:
-            posX -= samm
-    elif suund == "üles":
-        if posY <= 0:
-            suund = "peatatud"  
-        else:
-            posY -= samm
-
-    if suund != "peatatud":  
-        ekraan.blit(pudge,(posX,posY))
+    ekraan.blit(pudge,(posX,posY))
+    posX-=samm
     pygame.display.flip()
     ekraan.fill(roheline)
 pygame.quit()
